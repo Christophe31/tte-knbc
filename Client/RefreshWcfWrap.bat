6bea952936af363a230850c435b1906c67a3adf7
Christophe31 <Christophe31@abe22514-a19b-2d33-4b24-1dd898770ee2>
2010-04-29 12:37:44 +0000

    wshttpbinding->httpcontextbinding and bat script to renew client wraper

---
+++ Client/RefreshWcfWrap.bat
@@ -0,0 +1,9 @@
+rem ##########################
+rem # note: le service (BusinessLayer.svc) doit être lancé avant d'utiliser ce script depuis son dossier courrant.
+rem ##########################
+
+"c:\Program Files\Microsoft SDKs\Windows\v6.0A\Bin\SvcUtil.exe" http://localhost:2900/BusinessLayer.svc?wsdl
+del App.config
+del BusinessLayerWraper.cs
+rename BusinessLayer.cs BusinessLayerWraper.cs
+rename output.config App.config
