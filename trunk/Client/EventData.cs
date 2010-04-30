e716ccb83e652b60a9bb76cca7dddac72697ebf4
thomasdespax@hotmail.com <thomasdespax@hotmail.com@abe22514-a19b-2d33-4b24-1dd898770ee2>
2010-04-30 13:04:21 +0000

    2 class added. EventData --> Add EventData class partial between EventData Client and BusinessLayerWraper, Add CacheBusinessWraper for ICal

---
+++ trunk/Client/EventData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessLayer
+{
+    public partial class EventData : object, System.Runtime.Serialization.IExtensibleDataObject
+    {
+        public EventData()
+        {
+           
+        }
+        
+    }
+}
