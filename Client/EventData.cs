8261111fd446874b9c226857f8ed1a46c0b426a1
Christophe31 <Christophe31@abe22514-a19b-2d33-4b24-1dd898770ee2>
2010-05-03 13:50:52 +0000

    Automatise wrapping by "service references"

--- Client/EventData.cs
+++ Client/EventData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace BusinessLayer
+namespace Client.BusinessLayer
 {
     public partial class EventData : object, System.Runtime.Serialization.IExtensibleDataObject
     {
--- Client/RefreshWcfWrap.bat
+++
@@ -1,9 +0,0 @@
-rem ##########################
-rem # note: le service (BusinessLayer.svc) doit être lancé avant d'utiliser ce script depuis son dossier courrant.
-rem ##########################
-
-"c:\Program Files\Microsoft SDKs\Windows\v6.0A\Bin\SvcUtil.exe" http://localhost:2900/BusinessLayer.svc?wsdl
-del App.config
-del BusinessLayerWraper.cs
-rename BusinessLayer.cs BusinessLayerWrapper.cs
-rename output.config App.config
