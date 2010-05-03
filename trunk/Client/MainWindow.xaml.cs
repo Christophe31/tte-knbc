2fb0ca5ba8b93c69d07c1e2195e91b22c1f0ce28
Christophe31 <Christophe31@abe22514-a19b-2d33-4b24-1dd898770ee2>
2010-05-03 13:50:52 +0000

    Automatise wrapping by "service references"

--- trunk/Client/MainWindow.xaml.cs
+++ trunk/Client/MainWindow.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using BusinessLayer;
+using Client.BusinessLayer;
 
 namespace Client
 {
