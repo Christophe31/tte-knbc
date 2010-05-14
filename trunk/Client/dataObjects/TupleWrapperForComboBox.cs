6f928f186b76c41a9c336181fe1d2f604a4311df
Christophe31 <Christophe31@abe22514-a19b-2d33-4b24-1dd898770ee2>
2010-05-14 18:35:01 +0000

    add a way to keep ids in comboboxes with TupleWrapperForComboBox.

---
+++ trunk/Client/dataObjects/TupleWrapperForComboBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Client
+{
+	public class TupleWrapperForComboBox
+	{
+		public int Id;
+		public string Name;
+		public TupleWrapperForComboBox(Tuple<int,string> t)
+		{
+			Id = t.Item1;
+			Name = t.Item2;
+		}
+		public override string ToString()
+		{
+			return Name;
+		}
+	}
+}
