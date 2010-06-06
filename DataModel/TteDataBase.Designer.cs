5470f42f16eac36afca5cb74647e02674b5a1129
Christophe31 <Christophe31@abe22514-a19b-2d33-4b24-1dd898770ee2>
2010-06-06 14:50:59 +0000

    revert edmx things...

--- DataModel/TteDataBase.Designer.cs
+++ DataModel/TteDataBase.Designer.cs
@@ -23,7 +23,7 @@
 [assembly: global::System.Data.Objects.DataClasses.EdmRelationshipAttribute("TteDb", "FK_Droit_Utilisateur", "User", global::System.Data.Metadata.Edm.RelationshipMultiplicity.One, typeof(DataModel.User), "Right", global::System.Data.Metadata.Edm.RelationshipMultiplicity.Many, typeof(DataModel.Right))]
 
 // Original file name:
-// Generation date: 05/06/2010 17:41:55
+// Generation date: 01/06/2010 10:28:38
 namespace DataModel
 {
     
