f455c6261913c87675837393ade48884a1eadaa6
mugimasaka <mugimasaka@abe22514-a19b-2d33-4b24-1dd898770ee2>
2010-06-07 15:04:55 +0000

    Update .bat third attempt

--- trunk/BusinessLayer/CertificationGenerator.bat
+++ trunk/BusinessLayer/CertificationGenerator.bat
@@ -1,6 +1,7 @@
 ﻿rem 
 rem This command generate a new certificate
 rem
+makecert -sv SignRoot.pvk -cy authority -r signroot.cer -a sha1 -n "CN=Dev Certification Authority" -ss my -sr localmachine
 
-makecert -sv SignRoot.pvk -cy authority -r signroot.cer -a
-    sha1 -n "CN=localhost" -ss my -sr localmachine
+
+makecert -iv SignRoot.pvk -ic signroot.cer -cy end -pe -n CN="localhost" -eku 1.3.6.1.5.5.7.3.1 -ss my -sr localmachine -sky exchange -sp "Microsoft RSA SChannel Cryptographic Provider" -sy 12
