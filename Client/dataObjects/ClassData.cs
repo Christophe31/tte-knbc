a8b0aed09d4955919fd59e83126a72a6123dcdfe
Christophe31 <Christophe31@abe22514-a19b-2d33-4b24-1dd898770ee2>
2010-06-08 22:44:07 +0000

    fix SVN shits...

--- Client/dataObjects/ClassData.cs
+++ Client/dataObjects/ClassData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Client.BusinessLayer
+namespace Client.BusinessService
 {
 	public partial class ClassData : IdName, System.Runtime.Serialization.IExtensibleDataObject
 	{
