a19c7802d1cdfb08939ee514269dee9e33bd9612
mugimasaka <mugimasaka@abe22514-a19b-2d33-4b24-1dd898770ee2>
2010-06-12 17:15:04 +0000

    RoleData personnalisé

---
+++ trunk/Client/dataObjects/RoleData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Client.BusinessService;
+
+namespace Client.BusinessService
+{
+    public partial class RoleData : Object, System.Runtime.Serialization.IExtensibleDataObject
+    {
+        public string RoleName { get; set; }
+        public string TargetName { get; set; }
+    }
+}
